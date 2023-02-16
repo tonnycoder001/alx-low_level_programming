aa3980484a5ebf01af4d4cacfcfb5fef79ff0670
tonnycoder001 <tsandry7@gmail.com>
2023-02-16 01:35:28 -0800

    c printf/puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * Return: Always 0 (success)
  *
  */
-int main(void)
+int main (void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building multilingual puzzle");
 	return (0);
 }
