634bf245e312545550006849684e5503823c3240
tonnycoder001 <tsandry7@gmail.com>
2023-02-16 01:01:42 -0800

    first c programm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always B (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return 0;
+}
